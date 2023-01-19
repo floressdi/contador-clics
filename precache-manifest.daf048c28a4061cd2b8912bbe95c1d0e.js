f5261e267f60f31058506054fe89961f46b55397
Diego Flores <del.angelfull@gmail.com>
2023-01-18 21:30:59 -0600

    Updates

---
+++ precache-manifest.daf048c28a4061cd2b8912bbe95c1d0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8935cbad0b505a844bc1",
+    "url": "/contador-clics/static/css/main.692b8343.chunk.css"
+  },
+  {
+    "revision": "8935cbad0b505a844bc1",
+    "url": "/contador-clics/static/js/main.9a721f48.chunk.js"
+  },
+  {
+    "revision": "37f2ea0ceab704c66f7f",
+    "url": "/contador-clics/static/js/runtime~main.7a8a3d68.js"
+  },
+  {
+    "revision": "8484a4eacde89ec6c2fd",
+    "url": "/contador-clics/static/js/2.a879ad32.chunk.js"
+  },
+  {
+    "revision": "a06b242e230f66416d329540f7e004b2",
+    "url": "/contador-clics/index.html"
+  }
+];
